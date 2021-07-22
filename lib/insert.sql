b45bd3aa5b558a48de6470d480734544db2a61b1
Noam Lubofsky <noamlubofsky@gmail.com>
2021-07-22 17:09:17 -0400

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "M", "blue", "calm", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 10, "M", "red", "rowdy", false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 7, "F", "orange", "jumpy", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 8, "M", "brown", "tired", false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 4, "F", "beige", "excited", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 20, "M", "green", "grumpy", false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 9, "F", "purple", "happy", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 15, "M", "yellow", "angry", false);
